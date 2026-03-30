c99fefaadcdbeaef07d7e5133ef8a1f86c56d3bd
mustaalikanchwala <kanchwalamustaali@gmail.com>
2026-03-30 15:35:53 +0530

    storing the data

---
+++ biling-service/src/main/java/com/pm/biling_service/BilingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.biling_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingServiceApplication.class, args);
+	}
+
+}
